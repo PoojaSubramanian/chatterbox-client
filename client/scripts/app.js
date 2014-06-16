29ae29879003a9f3a04ed8f588f4c0ac0e9b5238
Hack Reactor Students <austentalbot@gmail.com>
2014-06-16 12:19:57 -0700

    passed fetch and send

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,42 @@
-// YOUR CODE HERE:
+var app={
+  init: function() {
+
+  },
+  send: function(message) {
+    $.ajax({
+      // always use this url
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  },
+  fetch: function() {
+    $.ajax({
+      // always use this url
+      //url: 'https://api.parse.com/1/classes/chatterbox/',
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message received');
+        console.log(data);
+      },
+      error: function (data) {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to get message');
+      }
+    });
+
+  }
+
+};
+
+
 
